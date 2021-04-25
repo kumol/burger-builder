f0b1259b89d65a730ba8ed35b3230f686247245c
kumol <kumol01763@gmail.com>
2021-04-26 01:38:41 +0600

    logo adding Header navigation toolbar added

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,10 +1,12 @@
 import Aux from "../../hoc/Aux";
 import React from "react";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import classes from "./Layout.module.css";
 const Layout = (props) =>{
     return (
         <Aux>
-            <div>App Drawer </div>
-            <main>{props.children}</main>
+            <Toolbar/>
+            <main className={classes.Content}>{props.children}</main>
         </Aux>
     );
 }
