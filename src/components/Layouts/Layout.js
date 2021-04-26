d49cd6b16743f3fc357e8aeddfba786372974647
kumol <kumol01763@gmail.com>
2021-04-27 04:25:11 +0600

    Side drawer

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,16 +1,31 @@
 import Aux from "../../hoc/Aux";
-import React from "react";
+import React, { Component } from "react";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.module.css";
 import Sidedrawer from "../Navigation/Sidedrawer/Sidedrawer";
-const Layout = (props) =>{
-    return (
-        <Aux>
-            <Toolbar/>
-            <Sidedrawer/>
-            <main className={classes.Content}>{props.children}</main>
-        </Aux>
-    );
+class Layout extends Component {
+    state={
+        showSideDrawer:true,
+    }
+    sideDrawerController = () =>{
+        this.setState({showSideDrawer:false});
+    }
+    sideDrawerControllerFromToolbar = () => {
+        this.setState((prevState)=>{
+            return{
+                showSideDrawer:!prevState.showSideDrawer
+            }
+        })
+    }
+    render(){
+        return (
+            <Aux>
+                <Toolbar clicked={this.sideDrawerControllerFromToolbar}/>
+                <Sidedrawer show={this.state.showSideDrawer} clicked={this.sideDrawerController}/>
+                <main className={classes.Content}>{this.props.children}</main>
+            </Aux>
+        );
+    }
 }
 
 export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../Sidedrawer/DrawerToggle/DrawerToggle";
 import classes from "./Toolbar.module.css";
 const Toolbar = (props) => (
     <header className = {classes.Toolbar}>
-        <div>Menu</div>
+        <DrawerToggle clicked={props.clicked}></DrawerToggle>
         <div className={classes.Logo}>
             <Logo/>
         </div>
