6ac6952cc1fe4c39901d205eaef245beb8bfb700
kumol <kumol01763@gmail.com>
2021-05-08 04:19:20 +0600

    dynamic selcect option input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,15 +3,22 @@ import classes from "./Input.module.css";
 
 const Input = (props) => {
     let inputArea=null;
-    switch(props.inputtype){
+    switch(props.elementType){
         case("input"):
-            inputArea = <input className={classes.InputElement} {...props}/>
+            inputArea = <input className={classes.InputElement} {...props.elementConfig}/>
             break;
         case("text-area"):
-            inputArea = <textarea className={classes.InputElement} {...props}/>
+            inputArea = <textarea className={classes.InputElement} {...props.elementConfig}/>
+            break;
+        case("select"):
+            inputArea = (<select className={classes.InputElement} value={props.value}>
+                {props.elementConfig.option.map((option,index)=>{
+                    return <option key={option.value+index} value={option.value}>{option.displayValue}</option>
+                })}
+            </select>)
             break;
         default:
-            inputArea = <input className={classes.InputElement} {...props}/>
+            inputArea = <input className={classes.InputElement} {...props.inputConfig}/>
     }
     return(
         <div className={classes.Input}>
--- src/container/Checkout/Contactdata/ContactData.js
+++ src/container/Checkout/Contactdata/ContactData.js
@@ -6,12 +6,46 @@ import Spinner from "../../../components/UI/Spinner/Spinner";
 import Input from "../../../components/UI/Input/Input";
 class ContactData extends Component {
     state={
-        name:"",
-        email:"",
-        price:"",
-        address:{
-            street:"",
-            postCode:""
+        orderForm:{
+            name:{
+                elementType:"input",
+                elementConfig:{
+                    type:"text",
+                    placeholder:"Your name"
+                },
+                value:""
+            },
+            email:{
+                elementType:"input",
+                elementConfig:{
+                    type:"text",
+                    placeholder:"Your email"
+                },
+                value:""
+            },
+            street:{
+                elementType:"input",
+                elementConfig:{
+                    type:"text",
+                    placeholder:"Street"
+                },
+                value:""
+            },
+            postCode:{
+                elementType:"input",
+                elementConfig:{
+                    type:"text",
+                    placeholder:"Post Code"
+                },
+                value:""
+            },
+            deliveryMethod:{
+                elementType:"select",
+                elementConfig:{
+                    option:[{value:"fastest",displayValue:"Fastest"},{value:"cheapest",displayValue:"Cheapest"}]
+                },
+                value:"fastest"
+            }
         },
         isLoading:false
     }
@@ -44,11 +78,23 @@ class ContactData extends Component {
         this.props.history.push("/");
     }
     render(){
+        const orderForm = [];
+        for(let key in this.state.orderForm){
+            orderForm.push({
+                id:key,
+                config:this.state.orderForm[key]
+            });
+        }
         const form = this.state.isLoading ? <Spinner/>:<form>
-            <Input inputtype="input" type="text" name="name" placeholder="Enter name"/>
+            {
+                orderForm.map(form=>{
+                    return <Input key={form.id} elementType={form.config.elementType} elementConfig={form.config.elementConfig} value={form.value}/>
+                })
+            }
+            {/* <Input elementType={this.state.elementType} elementConfig={this.state.elementConfig} value={this.state.value} />
             <Input  inputtype="input"  type="email" name="email" placeholder="Enter email"/>
             <Input  inputtype="input" type="text" name="street" placeholder="Enter street"/>
-            <Input  inputtype="input" type="text" name="postcode" placeholder="Enter postcode"/>
+            <Input  inputtype="input" type="text" name="postcode" placeholder="Enter postcode"/> */}
             <Button type="Success" clicked={this.confirmOrder}> Order  </Button>
         </form>;
         return(
