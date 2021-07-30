b41e5c6073606da29d70efa3bac2bcf04faaed68
kumol <kumol01763@gmail.com>
2021-07-30 23:31:52 +0600

    connecting react and redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App'
 import { BrowserRouter } from 'react-router-dom';
+import reducer from './store/reducer';
+import { createStore } from 'redux';
+import { Provider } from "react-redux"
 
-
+const store = createStore(reducer);
 const app = (
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <Provider store = {store}>
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+  </Provider>
 );
 ReactDOM.render(app,
   document.getElementById('root')
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from "./action";
+
+const initialState = {
+    totalPrice:0,
+    ingredients:null
+}
+
+const reducer = (state=initialState, action) => {
+
+}
+
+export default reducer;
